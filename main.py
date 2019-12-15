1377145e0ed91039d1220398424c3e2d00b77d14
ondatra <onigirionta@gmail.com>
2019-12-15 03:18:29 +0300

    Исправлен вывод в файл и проверка устойчивости

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from module import *
 from matplotlib import pylab as pl
 import os
+import time
 
 
 # Функция формирования выходного сигнала
@@ -42,11 +43,11 @@ def out_signal(xs, k1, k2, k3):
       ys.append(integrator_out)
     return ys
 
-# Функция проверки устойчивости по последним 40% отклика
+# Функция проверки устойчивости
 def control(ys):
     ref = ys[-1]
-    for i in range(int(len(ys)*0.4)):
-            if ref != ys[-(i+1)]:
+    for i in range(5):
+            if round(ref, 5) != round(ys[-(i+1)], 5):
                 conclusion = 'Неустойчивая система'
                 break
             else:
@@ -54,15 +55,16 @@ def control(ys):
     return conclusion
 
 # Функция формирования графика
-def response_plot(deltas, ys, k1, k3, name, mode):
+def response_plot(xs, ys, k1, k3, name, mode):
     stability = control(y)
-    pl.plot(deltas, ys)
+    pl.plot(xs, ys)
     format_str = 'Выходной сигнал при k1 = {} и k3 = {}\n{}'.format(k1, k3, stability)
     pl.title(format_str)
     pl.xlabel('Время')
     pl.ylabel('Выход')
     pl.grid()
-    pl.savefig(name, format='png', dpi=1000)
+    fig = pl.gcf()
+    fig.savefig(name, format='png')
     if mode == 'show':
         pl.show()
     pl.clf()
@@ -100,14 +102,14 @@ record_in_file(filename, x_time, x, y)
 
 # График
 mode = 'show'
-plotname = 'Plot:k1={}k3={}.png'.format(k1, k3)
+plotname = 'Plot_k1={}_k3={}.png'.format(k1, k3)
 response_plot(x_time, y, k1, k3, plotname, mode) 
 
 # Исследование областей
 
 # Формирование области исследования
-k1_list = list(range(-2, 3, 1))
-k3_list = list(range(-2, 3, 1))
+k1_list = list(range(-1, 2, 1))
+k3_list = list(range(-1, 2, 1))
 
 # Создание и переход в рабочую папку
 dir_name = input('Введите имя папки для складывания результатов: ')
@@ -118,8 +120,8 @@ mode = 'not show'
 for k1 in k1_list:
     for k3 in k3_list:
         y = out_signal(x, k1, k2, k3)
-        filename = 'Output:k1={}k3={}.txt'.format(k1, k3)
-        plotname = 'Plot:k1={}k3={}.png'.format(k1, k3)
+        filename = 'Output_k1={}_k3={}.txt'.format(k1, k3)
+        plotname = 'Plot_k1={}_k3={}.png'.format(k1, k3)
         record_in_file(filename, x_time, x, y)
         response_plot(x_time, y, k1, k3, plotname, mode)
         
